fe638133d1e0054f25dc2a77f29a97893b43d57a
Lukasz Machura <lukasz.machura@gmail.com>
2021-12-27 22:51:39 +0100

    structure

--- pzkapp/schema.sql
+++ pzkapp/schema.sql
@@ -12,38 +12,38 @@ CREATE TABLE club (
     www TEXT,
     fb TEXT,
     licence TEXT,
-    magic TEXT UNIQUE NOT NULL  /*unique number for registration*/
+magic TEXT UNIQUE NOT NULL  /*unique number for registration*/
 );
 
 CREATE TABLE member (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL,
-  first_name TEXT,
-  email TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL,
-  club TEXT NOT NULL,
-  club_id UNSIGNED SMALLINT NOT NULL,
-  pesel CHAR(11) NOT NULL DEFAULT "01010100000",
-  address TEXT,
-  phone TEXT,
-  student BOOLEAN DEFAULT 0,
-  kendo_grade TEXT,
-  iaido_grade TEXT,
-  jodo_grade TEXT,
-  kendo_licence TEXT,
-  iaido_licence TEXT,
-  jodo_licence TEXT,
-  events TEXT,
-  past_events TEXT,
-  pzk TEXT,  -- funkcja w PZK
-  kendo_instructor BOOLEAN DEFAULT 0,
-  iaido_instructor BOOLEAN DEFAULT 0,
-  jodo_instructor BOOLEAN DEFAULT 0,
-  kendo_coach BOOLEAN DEFAULT 0,
-  iaido_coach BOOLEAN DEFAULT 0,
-  jodo_coach BOOLEAN DEFAULT 0,
-  delegate BOOLEAN DEFAULT 0,
-  FOREIGN KEY (club_id) REFERENCES club (id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    first_name TEXT,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    club TEXT NOT NULL,
+    club_id UNSIGNED SMALLINT NOT NULL,
+    pesel CHAR(11) NOT NULL DEFAULT "01010100000",
+    address TEXT,
+    phone TEXT,
+    student BOOLEAN DEFAULT 0,
+    kendo_grade TEXT,
+    iaido_grade TEXT,
+    jodo_grade TEXT,
+    kendo_licence TEXT,
+    iaido_licence TEXT,
+    jodo_licence TEXT,
+    events TEXT,
+    past_events TEXT,
+    pzk TEXT,  -- funkcja w PZK
+    kendo_instructor BOOLEAN DEFAULT 0,
+    iaido_instructor BOOLEAN DEFAULT 0,
+    jodo_instructor BOOLEAN DEFAULT 0,
+    kendo_coach BOOLEAN DEFAULT 0,
+    iaido_coach BOOLEAN DEFAULT 0,
+    jodo_coach BOOLEAN DEFAULT 0,
+    delegate BOOLEAN DEFAULT 0,
+    FOREIGN KEY (club_id) REFERENCES club (id)
 );
 
 CREATE TABLE event (
